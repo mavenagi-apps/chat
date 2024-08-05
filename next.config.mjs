f30e6a6e130e5a023134ef6142d6862ec537a5d4
bsadecki-magi <bsadecki@mavenagi.com>
2024-08-05 15:37:09 -0700

    Add mavenagi config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  config: {
+    mavenagi: {
+      appType: 'next',
+    }
+  }
+};
 
 export default nextConfig;
