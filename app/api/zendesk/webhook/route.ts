474e3ce7c1883ef9ea482c02de25da0c24191abf
bsadecki-magi <bsadecki@mavenagi.com>
2024-10-16 13:42:32 -0700

    Create zendesk webhook

---
+++ app/api/zendesk/webhook/route.ts
@@ -0,0 +1,8 @@
+import { type NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+  console.log(req.body);
+  return NextResponse.json({
+    message: 'Webhook received',
+  });
+}
