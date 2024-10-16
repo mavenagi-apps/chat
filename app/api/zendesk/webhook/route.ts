66a38c0aa7f3831a1339c7cbe40416d48f707fd9
bsadecki-magi <bsadecki@mavenagi.com>
2024-10-16 13:47:12 -0700

    Update zendesk webhook

--- app/api/zendesk/webhook/route.ts
+++ app/api/zendesk/webhook/route.ts
@@ -1,7 +1,8 @@
 import { type NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
-  console.log(req.body);
+  const response = await req.json();
+  console.log(response);
   return NextResponse.json({
     message: 'Webhook received',
   });
