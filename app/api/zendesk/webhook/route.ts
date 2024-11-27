6883ce372271c2ac986dd62326e633d3e6b9bb3e
bsadecki-magi <bsadecki@mavenagi.com>
2024-11-27 14:50:13 -0800

    feat: feed webhook to redis

--- app/api/zendesk/webhook/route.ts
+++ app/api/zendesk/webhook/route.ts
@@ -1,9 +1,28 @@
 import { type NextRequest } from 'next/server';
-
 import { NextResponse } from 'next/server';
+import redisClient from '@/app/api/server/lib/redis';
 
 export const POST = async (request: NextRequest) => {
   const body = await request.json();
   console.log(JSON.stringify(body, null, 2));
-  return NextResponse.json({ message: 'Hello, world!' });
+  for (const event of body.events) {
+    if (!event.type.startsWith('conversation.')) {
+      continue;
+    }
+
+    const conversationId = event.payload?.conversation?.id;
+    const eventId = event.id;
+
+    if (!conversationId || !eventId) {
+      continue;
+    }
+
+    await redisClient.setEx(
+      `zendesk:${conversationId}:${eventId}`,
+      60, // 1 minute
+      JSON.stringify(event)
+    );
+  }
+
+  return NextResponse.json({ success: true });
 };
