65cf185378f48436fbccd376a439a7aa4ff1c3d0
bsadecki-magi <bsadecki@mavenagi.com>
2024-12-03 11:08:32 -0800

    feat!: add embed/iframe allowlist to app settings (#17)

--- settings.d.ts
+++ settings.d.ts
@@ -1,11 +1,14 @@
 declare global {
   interface AppSettings {
-    logoUrl: string;
-    brandColor: string;
-    amplitudeApiKey: string;
-    popularQuestions: string;
-    jwtPublicKey: string;
-    encryptionSecret: string;
+    logoUrl?: string;
+    brandColor?: string;
+    brandFontColor?: string;
+    amplitudeApiKey?: string;
+    popularQuestions?: string;
+    jwtPublicKey?: string;
+    encryptionSecret?: string;
+    embedAllowlist?: string[];
+    enableDemoSite?: string;
   }
 }
 
