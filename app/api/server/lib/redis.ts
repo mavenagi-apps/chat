1379c52a056e3f973644d35f55d7b21ab8232048
Jorge Mendez Cruz <jorge@mavenagi.com>
2024-12-10 12:49:08 -0800

    fix: create separate publish and subscribe clients

--- app/api/server/lib/redis.ts
+++ app/api/server/lib/redis.ts
@@ -1,13 +1,15 @@
 import { createClient } from "redis";
 
 // Create a Redis client factory
-const createRedisClient = async () => {
+const createRedisClient = async (mode?: "Publish" | "Subscribe") => {
   const client = createClient({
     url: process.env.STORAGE_REDIS_URL,
   });
 
-  client.on("error", (err) => console.error("Redis Client Error", err));
-  client.on("ready", () => console.log("Redis Client Ready"));
+  client.on("error", (err) =>
+    console.error("Redis Client Error", { mode }, err),
+  );
+  client.on("ready", () => console.log("Redis Client Ready", { mode }));
 
   await client.connect();
 
@@ -15,12 +17,20 @@ const createRedisClient = async () => {
 };
 
 const globalForRedis = global as unknown as {
-  redis: ReturnType<typeof createClient> | undefined;
+  redisPublish: ReturnType<typeof createClient> | undefined;
+  redisSubscribe: ReturnType<typeof createClient> | undefined;
 };
 
-export const getRedisClient = async () => {
-  if (!globalForRedis.redis) {
-    globalForRedis.redis = await createRedisClient();
+export const getRedisPublishClient = async () => {
+  if (!globalForRedis.redisPublish) {
+    globalForRedis.redisPublish = await createRedisClient("Publish");
   }
-  return globalForRedis.redis;
+  return globalForRedis.redisPublish;
+};
+
+export const getRedisSubscribeClient = async () => {
+  if (!globalForRedis.redisSubscribe) {
+    globalForRedis.redisSubscribe = await createRedisClient("Subscribe");
+  }
+  return globalForRedis.redisSubscribe;
 };
