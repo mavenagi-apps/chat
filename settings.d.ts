f791618a720f58c9319872d55bf2fa56a749b715
Brian Sadecki <bsadecki@mavenagi.com>
2024-12-16 12:47:05 -0800

    [feat] Customizable welcome message (#40)

--- settings.d.ts
+++ settings.d.ts
@@ -10,6 +10,7 @@ declare global {
     handoffConfiguration?: string;
     embedAllowlist?: string[];
     enableDemoSite?: string;
+    welcomeMessage?: string;
   }
   type BaseHandoffConfiguration = {
     apiKey: string;
