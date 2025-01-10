7b9ca27bce13f7c851e718c415a202a320d48c9a
bsadecki-magi <bsadecki@mavenagi.com>
2025-01-10 12:01:07 -0800

    chore: update handoff to strategy pattern

---
+++ lib/handoff/HandoffStrategyFactory.ts
@@ -0,0 +1,18 @@
+import type { HandoffStrategy } from "./HandoffStrategy";
+import { ZendeskStrategy } from "./ZendeskStrategy";
+import { FrontStrategy } from "./FrontStrategy";
+
+export type HandoffType = "zendesk" | "front" | null;
+
+export class HandoffStrategyFactory {
+  static createStrategy(type: HandoffType): HandoffStrategy | null {
+    switch (type) {
+      case "zendesk":
+        return new ZendeskStrategy();
+      case "front":
+        return new FrontStrategy();
+      default:
+        return null;
+    }
+  }
+}
