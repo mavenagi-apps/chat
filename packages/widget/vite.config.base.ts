a0dd637455fe1a0139ee179b2ff2ad1bd6bd3163
bsadecki-magi <bsadecki@mavenagi.com>
2025-01-28 18:37:42 -0800

    fix: update iframe domain envar for branch deploys

--- packages/widget/vite.config.base.ts
+++ packages/widget/vite.config.base.ts
@@ -6,20 +6,34 @@ import RootPath from "app-root-path";
 
 const PROJECT_ROOT = RootPath.toString();
 export const EXPORT_NAME = "widget";
+const DEFAULT_IFRAME_DOMAIN = "chat.onmaven.app";
+
+const generateIframeDomain = (
+  env: string,
+  generatedDeploymentUrl: string,
+  productionUrl: string,
+) => {
+  const domain = env === "production" ? productionUrl : generatedDeploymentUrl;
+  return domain || DEFAULT_IFRAME_DOMAIN;
+};
 
 export function createBaseConfig(mode: string): UserConfig {
-  const { VERCEL_PROJECT_PRODUCTION_URL } = loadEnv(
+  const { VERCEL_PROJECT_PRODUCTION_URL, VERCEL_ENV, VERCEL_URL } = loadEnv(
     mode,
     PROJECT_ROOT,
-    "VERCEL_PROJECT_PRODUCTION_URL",
+    ["VERCEL_PROJECT_PRODUCTION_URL", "VERCEL_ENV", "VERCEL_URL"],
+  );
+
+  const iframeDomain = generateIframeDomain(
+    VERCEL_ENV,
+    VERCEL_URL,
+    VERCEL_PROJECT_PRODUCTION_URL,
   );
 
   return {
     plugins: [preact()],
     define: {
-      __IFRAME_DOMAIN__: JSON.stringify(
-        VERCEL_PROJECT_PRODUCTION_URL || "chat.onmaven.app",
-      ),
+      __IFRAME_DOMAIN__: JSON.stringify(iframeDomain),
     },
     build: {
       lib: {
